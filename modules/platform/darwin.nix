dc16ddb2333b49e6ec16d94338cdf287abd0465c
tyPhoon <typhoonmoon1022@gmail.com>
2026-05-13 21:10:31 +0900

    feat: macismの統合

--- modules/platform/darwin.nix
+++ modules/platform/darwin.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     ../programs/aerospace.nix
     ../programs/karabiner.nix
+  ];
+
+  home.packages = lib.optionals config.core.system.desktop [
+    pkgs.macism
   ];
 }
