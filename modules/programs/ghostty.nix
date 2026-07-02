aa3e830d3cd1f8e2cb7a3ba1cf36e7ae3e86ab5e
tyPhoon <typhoonmoon1022@gmail.com>
2026-07-03 01:08:51 +0900

    feat: QuickTerminalのサイズを指定

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -22,6 +22,7 @@ in
       keybind = super+ctrl+f=unbind
       keybind = global:super+space=toggle_quick_terminal
       quick-terminal-position = center
+      quick-terminal-size = 70%,80%
       quick-terminal-animation-duration = 0
       macos-titlebar-style = hidden
       confirm-close-surface = false
